13f852a55eb573646c1b2aaf8c8c456d65f60cda
JEAD <jeadz1234@gmail.com>
2023-05-20 01:04:23 -0500

    Post

--- client-project/src/pages/Main/Main.js
+++ client-project/src/pages/Main/Main.js
@@ -5,7 +5,7 @@ export const Main = () => {
   return (
     <div className='main-form'>
       <h1>Welcome to My App</h1>
-      <p>Get started by signing up or logging in</p>
+      <p>Gret stated by signing up or logging in</p>
     </div>
   );
 };
